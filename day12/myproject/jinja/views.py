8363526880e2c6267adc9e44bff1922205885f93
Shashi Kumar <kumar7d1432@gmail.com>
2025-07-12 15:22:35 +0530

    Add files via upload

---
+++ day12/myproject/jinja/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+def displaygreen(request):
+    stu=[{'name':'shiva','marks':90},{'name':'navmohan','marks':85},
+         {'name':'shravani','marks':88},{'name':'salma','marks':91}]
+
+    return render(request,'green.html',{'data': stu})
+
+##def displaygreen(request):
+##    stu=['shiva','navmohan','shravani','salma']
+##    return render(request,'green.html',{'data': stu})
